6db9861fa6647d742248caf8e8a8f214bb52a792
Alexis de Jesus Torres López <152741790+AlexReckless@users.noreply.github.com>
2024-05-16 12:39:03 -0600

    Add files via upload
    Advice practice

---
+++ example01.js
@@ -0,0 +1,38 @@
+//var name2="Alexis";
+//console. log(name2);
+//alert(name2);  
+
+//var name2=5;
+//alert(name2);
+
+//cual es la diferencia entre let,var y const?
+//respuesta: ninguna;
+
+//var var1=10;
+//let let1=10;
+//const const1=10;
+
+//console. log(var1);
+//console. log(let1);
+//console. log(const1);
+//console. log(var1+let1+const1)
+
+function hello(){
+    //first may
+   // var age=$("#age");
+
+    //second way
+    var age=document.getElementById("age");
+alert("hello mr x, your age is: "+age.value);
+
+var age=document.getElementById("nombre");
+alert("hello mr x, your age is: "+nombre.value);
+
+var age=document.getElementById("hobby");
+alert("hello mr x, your age is: "+hobby.value);
+
+var age=document.getElementById("favoritefood");
+alert("hello mr x, your age is: "+favoritefood.value);
+
+}
+
